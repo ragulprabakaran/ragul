ed43701454b69c0a361a0732d382a922db9d9a8e
ragulprabakaran <ragulprabakaran@gmail.com>
2017-06-24 12:49:06 +0530

    Update Prime3.java

--- Prime3.java
+++ Prime3.java
@@ -1,13 +1,17 @@
 
 
 
-
+import java.util.Scanner;
 public class Prime3 {
 public static void main(String args[])
 {
 	int no;
+	int a,b;
+	Scanner in=new Scanner(System.in);
+	a=in.nextInt();
+	b=in.nextInt();
 	System.out.println("The Prime number between 10 to 99 is ");
-	for(no=10;no<=99;no++)
+	for(no=a;no<=b;no++)
 	{boolean isPrime=true;
 		for(int j=2;j<no;j++)
 	{
